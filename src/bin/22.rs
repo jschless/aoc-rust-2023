b40a155b1cebed5845072fbd1ee5920168444096
jschless <joe.c.schlessinger@gmail.com>
2023-12-22 13:42:36 -0500

    refactored to be good

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -10,80 +10,23 @@ struct Brick {
 }
 
 impl Brick {
-    fn lowest_point(&self) -> isize {
-        std::cmp::min(self.start.z, self.end.z)
-    }
-
-    fn on_ground(&self) -> bool {
-        self.lowest_point() == 1
-    }
-
-    fn fall(&self) -> Self {
-        Self {
-            start: self.start.fall(),
-            end: self.end.fall(),
-            id: self.id,
+    fn points(&self) -> Vec<Point> {
+        let mut points: Vec<Point> = Vec::new();
+        for x in std::cmp::min(self.start.x, self.end.x)..=std::cmp::max(self.start.x, self.end.x) {
+            for y in
+                std::cmp::min(self.start.y, self.end.y)..=std::cmp::max(self.start.y, self.end.y)
+            {
+                for z in std::cmp::min(self.start.z, self.end.z)
+                    ..=std::cmp::max(self.start.z, self.end.z)
+                {
+                    points.push(Point { x, y, z });
+                }
+            }
         }
-    }
-
-    fn on_segment(p: Point, q: Point, r: Point) -> bool {
-        (q.x <= p.x.max(r.x) && q.x >= p.x.min(r.x)) && (q.y <= p.y.max(r.y) && q.y >= p.y.min(r.y))
-    }
-
-    fn orientation(p: Point, q: Point, r: Point) -> i32 {
-        let val = ((q.y - p.y) * (r.x - q.x)) - ((q.x - p.x) * (r.y - q.y));
-
-        if val == 0 {
-            return 0; // colinear
-        }
-
-        if val > 0 {
-            return 1; // clockwise
-        }
-
-        2 // counterclockwise
-    }
-
-    fn do_intersect(p1: Point, q1: Point, p2: Point, q2: Point) -> bool {
-        let o1 = Brick::orientation(p1, q1, p2);
-        let o2 = Brick::orientation(p1, q1, q2);
-        let o3 = Brick::orientation(p2, q2, p1);
-        let o4 = Brick::orientation(p2, q2, q1);
-
-        if o1 != o2 && o3 != o4 {
-            return true;
-        }
-
-        if o1 == 0 && Brick::on_segment(p1, p2, q1) {
-            return true;
-        }
-
-        if o2 == 0 && Brick::on_segment(p1, q2, q1) {
-            return true;
-        }
-
-        if o3 == 0 && Brick::on_segment(p2, p1, q2) {
-            return true;
-        }
-
-        if o4 == 0 && Brick::on_segment(p2, q1, q2) {
-            return true;
-        }
-
-        false
-    }
-
-    fn is_touching(&self, b: Brick) -> bool {
-        if std::cmp::min(self.start.z, self.end.z) >= std::cmp::min(b.start.z, b.end.z)
-            && std::cmp::min(self.start.z, self.end.z) <= std::cmp::max(b.start.z, b.end.z)
-        {
-            Brick::do_intersect(self.start, self.end, b.start, b.end)
-        } else {
-            false
-        }
+        points
     }
 }
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 struct Point {
     x: isize,
     y: isize,
@@ -97,14 +40,6 @@ impl Point {
         let y = coords[1].parse::<isize>().unwrap();
         let z = coords[2].parse::<isize>().unwrap();
         Point { x, y, z }
-    }
-
-    fn fall(&self) -> Self {
-        Point {
-            x: self.x,
-            y: self.y,
-            z: self.z - 1,
-        }
     }
 }
 
@@ -121,49 +56,68 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .collect();
 
-    bricks.sort_by_key(|brick| brick.lowest_point());
+    bricks.sort_by_key(|brick| std::cmp::min(brick.start.z, brick.end.z));
 
-    let mut new_bricks: Vec<Brick> = Vec::new();
-    let mut brick_supporters: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut brick_supported: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut brick_supporters: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut brick_supported: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut z_map: HashMap<(isize, isize), isize> = HashMap::new();
+    let mut grid: HashMap<Point, usize> = HashMap::new();
 
     for b in bricks {
-        brick_supporters.insert(b.id, Vec::new());
-        brick_supported.insert(b.id, Vec::new());
-        let mut poss_brick = b.clone();
-        loop {
-            let mut flag = false;
-            for existing_brick in new_bricks.clone() {
-                if poss_brick.fall().is_touching(existing_brick.clone()) {
-                    flag = true;
-                    brick_supporters
-                        .get_mut(&existing_brick.id)?
-                        .push(poss_brick.id);
+        brick_supporters.insert(b.id, HashSet::new());
+        brick_supported.insert(b.id, HashSet::new());
+        let points = b.points();
 
-                    brick_supported
-                        .get_mut(&poss_brick.id)?
-                        .push(existing_brick.id);
-                }
+        // find z at highest collision
+        let new_z = points
+            .iter()
+            .map(|p| *z_map.entry((p.x, p.y)).or_insert(0))
+            .max()?
+            + 1;
+
+        let b_bottom = std::cmp::min(b.start.z, b.end.z);
+
+        // loop through all points for this brick
+        for p in points {
+            let possible_collision_point = Point {
+                x: p.x,
+                y: p.y,
+                z: new_z - 1,
+            };
+            // if there is a brick at this grid, update the supporters/supportin
+            if let Some(existing_brick_id) = grid.get(&possible_collision_point) {
+                brick_supporters
+                    .entry(*existing_brick_id)
+                    .or_insert(HashSet::new())
+                    .insert(b.id);
+                brick_supported
+                    .entry(b.id)
+                    .or_insert(HashSet::new())
+                    .insert(*existing_brick_id);
             }
 
-            if flag || poss_brick.on_ground() {
-                new_bricks.push(poss_brick.clone());
-                break;
-            }
-
-            poss_brick = poss_brick.fall();
+            // update this position to be this grid
+            z_map.insert((p.x, p.y), new_z + (p.z - b_bottom));
+            grid.insert(
+                Point {
+                    x: p.x,
+                    y: p.y,
+                    z: new_z + (p.z - b_bottom),
+                },
+                b.id,
+            );
         }
     }
+
     let mut count = 0;
-    let mut bricks: Vec<_> = Vec::new();
-    for (b, supported) in brick_supporters.clone() {
+    for (_b, supported) in brick_supporters.clone() {
         // for each supporting brick B1, check for all the bricks Bn its supporting that # supporters(Bn) != 1
         if supported.iter().all(|s| brick_supported[s].len() != 1) {
             count += 1;
-            bricks.push(b);
         }
     }
     Some(count)
+    // None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
