a19b51045539ae73d25187115ec7280724199614
jschless <joe.c.schlessinger@gmail.com>
2023-12-25 11:09:32 -0500

    solved d21 p2

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, VecDeque};
 
 advent_of_code::solution!(21);
 
@@ -22,34 +22,77 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
-    let s = explore_dec(64, start_loc, &grid_map);
-    Some(s.len() as u32)
+    let visited = explore_dec(start_loc, &grid_map);
+    Some(
+        visited
+            .values()
+            .filter(|v| **v % 2 == 0 && **v <= 64)
+            .count() as u32,
+    )
 }
 
 fn explore_dec(
-    steps: usize,
     start: (isize, isize),
     grid: &HashMap<(isize, isize), char>,
-) -> HashSet<(isize, isize)> {
-    let mut q: HashSet<(isize, isize)> = HashSet::new();
-    q.insert(start);
-    for _ in 0..steps {
-        let mut next_q: HashSet<(isize, isize)> = HashSet::new();
-        for last in q {
-            for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let next = (last.0 + dx, last.1 + dy);
-                if grid.contains_key(&next) && next != last && grid[&next] != '#' {
-                    next_q.insert(next);
-                }
+) -> HashMap<(isize, isize), usize> {
+    let mut visited: HashMap<(isize, isize), usize> = HashMap::new();
+    let mut to_explore: VecDeque<((isize, isize), usize)> = VecDeque::from([(start, 0)]);
+    while let Some((last, dist)) = to_explore.pop_front() {
+        for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+            let next = (last.0 + dx, last.1 + dy);
+            if grid.contains_key(&next) && grid[&next] != '#' && !visited.contains_key(&next) {
+                to_explore.push_back((next, dist + 1));
+                visited.insert(next, dist + 1);
             }
         }
-        q = next_q.clone();
     }
-    q
+
+    visited
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut grid_map: HashMap<(isize, isize), char> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(x, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(y, c)| ((x as isize, y as isize), c))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let mut start_loc = (0, 0);
+
+    for (key, val) in &grid_map {
+        if val == &'S' {
+            start_loc = *key;
+        }
+    }
+    grid_map.insert(start_loc, '.');
+
+    let grid_max = grid_map.keys().map(|k| k.1).max()? + 1;
+
+    let visited = explore_dec(start_loc, &grid_map);
+    let even_corners = visited
+        .values()
+        .filter(|v| **v % 2 == 0 && **v > 65)
+        .count();
+
+    let odd_corners = visited
+        .values()
+        .filter(|v| **v % 2 == 1 && **v > 65)
+        .count();
+
+    let even_full = visited.values().filter(|v| **v % 2 == 0).count();
+    let odd_full = visited.values().filter(|v| **v % 2 == 1).count();
+
+    let n = ((26501365 - (grid_max / 2)) / grid_max) as usize;
+
+    let p2 = ((n + 1) * (n + 1)) * odd_full + (n * n) * even_full - (n + 1) * odd_corners
+        + n * even_corners;
+
+    Some(p2 as u64)
 }
 
 #[cfg(test)]
@@ -59,7 +102,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(16));
+        assert_eq!(result, Some(42));
     }
 
     #[test]
